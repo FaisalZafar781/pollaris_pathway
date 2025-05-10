c7762eac7bd0094d048dfc1410ba4a9c4384feec
Hamzayaqoob007 <Hamza.yaqoob@swyftbooking.com>
2025-05-10 06:37:50 +0500

    add super user

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,4 +13,18 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+def create_superuser():
+    import django
+    django.setup()
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin12345")
+        print("Superuser created")
+    else:
+        print("Superuser already exists")
+
+create_superuser() 
+
 application = get_wsgi_application()
