7c93855757bed246bf9382b217361fa27849eb61
Hamzayaqoob007 <Hamza.yaqoob@swyftbooking.com>
2025-05-10 06:43:01 +0500

    add migrationsr

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,6 +12,16 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+def apply_migrations():
+    import django
+    django.setup()
+    from django.core.management import call_command
+
+    try:
+        call_command('migrate')
+        print("Migrations applied")
+    except Exception as e:
+        print(f"Migration error: {e}")
 
 def create_superuser():
     import django
@@ -25,6 +35,8 @@ def create_superuser():
     else:
         print("Superuser already exists")
 
-create_superuser() 
+# create_superuser() 
+apply_migrations()
+
 
 application = get_wsgi_application()
