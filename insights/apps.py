4aba4db7a19351a15538b1f8d967e0f26f10dda2
Hamzayaqoob007 <Hamza.yaqoob@swyftbooking.com>
2025-05-10 06:14:42 +0500

    set data.json

--- insights/apps.py
+++ insights/apps.py
@@ -7,11 +7,3 @@ class InsightsConfig(AppConfig):
 
     def ready(self):
         import insights.signals
-        from django.contrib.auth import get_user_model
-        from django.core.management import call_command
-        from django.db.utils import OperationalError
-        import os
-
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin123")
